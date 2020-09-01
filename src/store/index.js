242b863665cb4938080b9bc3c2e366f25789c43d
musfirotus <musfirotus@gmail.com>
2020-09-02 01:46:20 +0700

    initial commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,81 +1,15 @@
-import Vue from "vue";
-import Vuex from "vuex";
-import createPersistedState from "vuex-persistedstate";
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-import router from "../router/index";
-
-import Api from "../service/api";
-
-Vue.use(Vuex);
-
-const Auth = {
-  namespaced: true,
-  state: () => ({
-    token: "",
-    user: {
-      id: "",
-      fullName: "",
-      phone: "",
-      role: "",
-      username: "",
-    },
-    isError: false,
-    errorMessage: "",
-  }),
-  mutations: {
-    saveLogin(state, payload) {
-      state.token = payload.token;
-      state.user = {
-        id: payload.id,
-        fullName: payload.full_name,
-        phone: payload.phone_number,
-        role: payload.role,
-        username: payload.username,
-      };
-    },
-  },
-  actions: {
-    async reqLogin({ commit }, payload) {
-      // console.log({ reqLogin: true });
-      Api.post("/auth/login", {
-        data: payload,
-      })
-        .then((res) => {
-
-          const {
-            data: { data },
-          } = res;
-          commit("saveLogin", data);
-          localStorage.setItem("token", data.token);
-          localStorage.setItem(
-            "user",
-            JSON.stringify({
-              id: data.id,
-              fullName: data.full_name,
-              phone: data.phone_number,
-              role: data.role,
-              username: data.username,
-            })
-          );
-          router.push("/dashboard");
-
-          // console.log({ res });
-        })
-        .catch((error) => {
-          console.log({ error });
-        });
-      // const { data } = await Api.post("/auth/login", { data: payload });
-      console.log({ commit });
-    },
-  },
-};
+Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {
-    Auth,
+  state: {
   },
-  plugins: [createPersistedState()],
-});
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
